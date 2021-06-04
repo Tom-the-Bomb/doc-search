fda572fc36e333911f7da5ca0bcd576c85839af0
Tom-the-Bomb <thello522@gmail.com>
2021-06-04 14:56:01 -0400

    bump version

--- doc_search/__init__.py
+++ doc_search/__init__.py
@@ -2,7 +2,7 @@ from .async_docs import AsyncScraper
 from .sync_docs  import SyncScraper
 from .utils import *
 
-__version__ = "1.0.7"
+__version__ = "1.0.8"
 
 __author__  = "Tom the Bomb"
 __license__ = "MIT"
